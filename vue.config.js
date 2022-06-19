80dea15209807e6176686a2497a5d5eb4bd36652
Toygar Saral <farewell1945@hotmail.com>
2022-06-19 19:08:40 +0300

    Add public repo path for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: "/weather-app-public",
   css: {
     loaderOptions: {
       sass: {
